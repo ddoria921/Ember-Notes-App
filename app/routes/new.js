be514c4150f0b84635f5544d6dbe36bbfd94f39c
Darin Doria <doria@knights.ucf.edu>
2016-03-20 18:34:56 -0400

    Fill in createdAt and updateAt when new note is created

--- app/routes/new.js
+++ app/routes/new.js
@@ -1,15 +1,17 @@
 import Ember from 'ember';
 
-var newNoteRoute = Ember.Route.extend({
-	model() {
-		return this.store.createRecord('note');
-	},
-	renderTemplate() {
-		this.render('note');
-	},
-	resetController(controller) {
-		controller.set('refreshEditorFlag', true);
-	}
+export default Ember.Route.extend({
+  model() {
+    return this.store.createRecord('note', {
+      note: 'New note mother....',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    });
+  },
+  renderTemplate() {
+    this.render('note');
+  },
+  resetController(controller) {
+    controller.set('refreshEditorFlag', true);
+  }
 });
-
-export default newNoteRoute;
