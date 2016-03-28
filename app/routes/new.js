79ceb5fe1cfcee9e25c69cf2ca0a36fe624e4284
Darin Doria <doria@knights.ucf.edu>
2016-03-27 23:41:40 -0400

    Redirect new route to the note that was just created.

--- app/routes/new.js
+++ app/routes/new.js
@@ -3,9 +3,17 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.createRecord('note', {
-      note: 'New note mother....',
+      body: '> new note...',
       createdAt: new Date(),
       updatedAt: new Date()
+    }).save();
+  },
+  afterModel(resolvedModel) {
+    this.controllerFor('application').set('activeNote', null);
+
+    resolvedModel.save().then((savedNote) => {
+      this.transitionTo('note', savedNote.get('id'));
+      this.controllerFor('application').set('activeNote', resolvedModel);
     });
   },
   renderTemplate() {
