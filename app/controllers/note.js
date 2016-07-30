49a20490e4a154b5776bf9c4cdf37812bbe9e4f6
Darin Doria <doria@knights.ucf.edu>
2016-07-30 16:26:23 -0400

    Changes to support didFinishTyping action in medium-editor component.

--- app/controllers/note.js
+++ app/controllers/note.js
@@ -1,27 +1,19 @@
 import Ember from 'ember';
-import { task } from 'ember-concurrency';
+import {
+  task
+} from 'ember-concurrency';
 
-var noteController = Ember.Controller.extend({
-	refreshEditorFlag: false,
-	isTyping: false,
-	isSaving: false,
-  promptDelete: false,
-
-	isTypingObserver: function() {
-		if (!this.get('isTyping')) {
-			this.send('save');
-		}
-	}.observes('isTyping'),
-
-  editorOptions: function() {
+export default Ember.Controller.extend({
+  editorOptions: function () {
     return {
-      buttons: ["bold", "italic"],
-      allowMultiParagraphSelection: true,
-      toolbar: false
+      toolbar: {
+        buttons: ["bold", "italic"],
+        allowMultiParagraphSelection: true
+      }
     };
   }.property(),
 
-  saveNote: task(function *() {
+  saveNote: task(function* () {
     if (this.get('model.isNew')) {
       this.set('model.createdAt', new Date());
     }
@@ -30,22 +22,7 @@ var noteController = Ember.Controller.extend({
     yield this.get('model').save();
   }).drop(),
 
-	actions: {
-		save: function() {
-      this.get('saveNote').perform();
-			// if (this.get('model.isNew')) {
-			// 	this.set('model.createdAt', new Date());
-			// }
-			// this.set('isSaving', true);
-      // this.set('model.updatedAt', new Date());
-      // this.get('model').save().then(() => {
-			// 	this.set('isSaving', false);
-			// });
-		},
-		refreshEditor: function() {
-			this.set('refreshEditorFlag', true);
-		}
-	}
-});
+  actions: {
 
-export default noteController;
+  }
+});
