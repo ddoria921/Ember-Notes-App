cb8c795670468cdf06e77593e267979e5f436672
Darin Doria <doria@knights.ucf.edu>
2016-08-07 18:28:57 -0400

    Redirect any from index to notes route.

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,10 +1,24 @@
-import Em from 'ember';
+import Ember from 'ember';
 
-export default Em.Route.extend({
+const {
+  Logger,
+  run
+} = Ember;
+
+export default Ember.Route.extend({
   beforeModel() {
     return this.get("session").fetch().catch((error) => {
-      Em.Logger.error('User not authenticated. Redirecting to sign in. Error:', error);
+      Logger.error('User not authenticated. Redirecting to sign in. Error:', error);
       this.transitionTo('signin');
+    });
+  },
+
+  redirect() {
+    run.next(this, () => {
+      const appController = this.controllerFor('application');
+      if (appController.get('currentPath') === 'index') {
+        this.transitionTo('notes');
+      }
     });
   },
 
