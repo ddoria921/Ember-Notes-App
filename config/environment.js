e00e03178e6c49700d0d6cc30da7d92e324c3706
Darin Doria <doria@knights.ucf.edu>
2016-08-07 18:51:31 -0400

    Changing locationType to ‘hash’ to fix refreshing

--- config/environment.js
+++ config/environment.js
@@ -22,7 +22,7 @@ module.exports = function(environment) {
       preventDuplicates: true
     },
     rootURL: '/',
-    locationType: 'auto',
+    locationType: 'hash',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -47,7 +47,7 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/';
+    ENV.rootURL = '/';
     ENV.locationType = 'none';
 
     // keep test console output quieter
